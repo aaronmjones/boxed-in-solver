842107bf69f7604abe809911d63437d96c85afff
Aaron Jones <ajones2279@gmail.com>
2017-11-20 08:44:53 -0700

    Use boost program_options instead of get_opt

--- src/BoxedInSolver.cc
+++ src/BoxedInSolver.cc
@@ -6,10 +6,7 @@
  * \copyright GNU Public License.
  */
 
-#if defined (_WIN32)
-# include <wingetopt.h>
-#elif defined (__linux__) || defined (__APPLE__)
-# include <getopt.h>
+#if defined (__linux__) || defined (__APPLE__)
 # include <signal.h>
 # include <execinfo.h> // backtrace
 #endif
@@ -17,22 +14,21 @@
 #include <cstdlib>
 #include <cstdio>
 #include <time.h>
-
 #include <fstream>
 #include <iostream>
-
+#include <string>
+#include <boost/program_options.hpp>
 #include <boxedinio.h>
 #include <astar.h>
-#include "Node.h"
-#include "Level.h"
-#include "Heuristic.h"
+#include <Heuristic.h>
+#include <Level.h>
+#include <Node.h>
 
 
 #if defined (__linux__) || defined (__APPLE__)
 static void signal_handler(int signum);
 #endif
 
-static void usage(void);
 
 using namespace std;
 using namespace boxedin;
@@ -40,151 +36,134 @@ using namespace boxedin;
 
 int main(int argc, char* argv[])
 {
-    char* stats_path = NULL;
-
+  string stats_path;
+  string level_path;
+  
 #if defined (__linux__) || defined (__APPLE__)
-    // Setup process signal handlers
-    signal(SIGSEGV, signal_handler);
+  // Setup process signal handlers
+  signal(SIGSEGV, signal_handler);
 #endif
 
-    // Handle command line options
-    while (1)
+  try {
+    boost::program_options::options_description desc("solve OPTIONS");
+    desc.add_options()
+      ("help,h",                                                                  "Display help"             )
+      ("color,c", boost::program_options::value<bool>(&boxedin::io::use_colors),  "Display level in color"   )
+      ("stats,s", boost::program_options::value<string>(&stats_path),             "Output stats file"        )
+      ("level,l",   boost::program_options::value<string>(&level_path)->required(), "Input boxed-in level file")
+      ;
+    
+    boost::program_options::positional_options_description positionalOptions;
+    positionalOptions.add("level", 1);
+        
+    boost::program_options::variables_map variablesMap;
+    boost::program_options::store(
+      boost::program_options::command_line_parser(argc, argv)
+      .options(desc)
+      .positional(positionalOptions)
+      .run(),
+      variablesMap );
+
+    if (variablesMap.count("help"))
     {
-        const char* short_options = "s:c";
-
-        int c = getopt(argc, argv, short_options);
-
-        // No more options
-        if (c == -1)
-            break;
-
-        switch (c)
-        {
-        case 's':
-            stats_path = optarg;
-            break;
-        case 'c':
-            boxedin::io::use_colors = true;
-            break;
-        default:
-            usage();
-        }
+      cerr << desc << endl;
+      return 0;
     }
 
-    // Handle remaining command line argument(s).
-    // If argument exists, its the level file; if no argument was given
-    // expect the level to come from stdin.
-    char* filename = NULL;
+    boost::program_options::notify(variablesMap);
+  }
+  catch (boost::program_options::error& e)
+  {
+    cerr << e.what() << std::endl;
+    return 1;
+  }
 
-    if (optind < argc)
-    {
-        filename = argv[optind];
-    }
+  // ---------------------------------------------------------------------------
 
-    // Parse the level
-    vector<vector<char> > charmap;
-    if (filename)
+  // Parse the level
+  vector<vector<char> > charmap;
+  if (!level_path.empty())
+  {
+    cerr << level_path << endl;
+    ifstream level_istream(level_path.c_str());
+    boxedin::io::ParseCharMap(level_istream, charmap);
+  }
+  else
+  {
+    fprintf(stderr, "ERROR: Please specify a level file\n");
+    return 1;
+  }
+
+  if (!boxedin::io::IsValidBoxedInLevel(charmap))
+  {
+    fprintf(stderr, "ERROR: Invalid boxed in level\n");
+    return 1;
+  }
+
+  cerr << "Boxed In Level:" << endl;
+  PrintCharMapInColor(cerr, charmap);
+
+  // TODO: trim unnecessary rows and columns and re-print the level
+    
+  // A* Search
+  time_t rawtime;
+  struct tm* timeinfo;
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  cerr << asctime(timeinfo) << endl;
+
+  Level level = Level::MakeLevel(charmap);
+
+  ShortestManhattenDistanceThroughGearsToExitHeuristic heuristic(level);
+  SearchResult result = astar(level, heuristic);
+    
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  cerr << asctime(timeinfo) << endl;
+
+  if (result.success)
+  {
+    cerr << "A* search succeeded" << endl;
+    // write stats to file or stderr
+    if (!stats_path.empty())
     {
-        ifstream level_istream(filename);
-        boxedin::io::ParseCharMap(level_istream, charmap);
+      ofstream stats_output(stats_path.c_str());
+      stats_output << result << endl;
     }
     else
     {
-        boxedin::io::ParseCharMap(cin, charmap);
+      cerr << result << endl;
     }
 
-    if (!boxedin::io::IsValidBoxedInLevel(charmap))
-    {
-        fprintf(stderr, "ERROR: Invalid boxed in level\n");
-        return 1;
-    }
+    // write solution to stdout
+    cout << result.solution << endl;
 
-    if (filename)
-    {
-        cerr << filename << endl;
-    }
-    cerr << "Boxed In Level:" << endl;
-    PrintCharMapInColor(cerr, charmap);
+    return 0;
+  }
 
-    // TODO: trim unnecessary rows and columns and re-print the level
-    
-    // A* Search
-    time_t rawtime;
-    struct tm* timeinfo;
-
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    cerr << asctime(timeinfo) << endl;
-
-    Level level = Level::MakeLevel(charmap);
-
-    ShortestManhattenDistanceThroughGearsToExitHeuristic heuristic(level);
-    SearchResult result = astar(level, heuristic);
-    
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    cerr << asctime(timeinfo) << endl;
-
-    if (result.success)
-    {
-        cerr << "A* search succeeded" << endl;
-        // write stats to file or stderr
-        if (stats_path)
-        {
-            ofstream stats_output(stats_path);
-            stats_output << result << endl;
-        }
-        else
-        {
-            cerr << result << endl;
-        }
-
-        // write solution to stdout
-        cout << result.solution << endl;
-
-        return 0;
-    }
-
-    cerr << "A* search failed" << endl;
-    return 1;
-}
-
-
-
-static void usage(void)
-{
-    fprintf(stderr,
-        "Usage:\n"
-        "BoxedInSolver OPTIONS file\n"
-        "  Reads a boxed in level from file and runs the solving\n"
-        "  algorithm for it.\n"
-        "  OPTIONS\n"
-        "    -c\n"
-        "      Print console output in color\n"
-        "    -s, <statsfile>\n"
-        "      Print solver stats to statsfile\n"
-        );
-    exit(1);
+  cerr << "A* search failed" << endl;
+  return 1;
 }
 
 
 #if defined (__linux__) || defined (__APPLE__)
 static void signal_handler(int signum)
 {
-    /* Print backtrace for seg fault */
-    if (signum == SIGSEGV)
-    {
-        void* tracePtrs[100];
-        int count = backtrace( tracePtrs, 100 );
-        char** funcNames = backtrace_symbols( tracePtrs, count );
+  /* Print backtrace for seg fault */
+  if (signum == SIGSEGV)
+  {
+    void* tracePtrs[100];
+    int count = backtrace( tracePtrs, 100 );
+    char** funcNames = backtrace_symbols( tracePtrs, count );
 
-        // Print the stack trace
-        for( int ii = 0; ii < count; ii++ )
-            cerr << funcNames[ii] << endl;;
+    // Print the stack trace
+    for( int ii = 0; ii < count; ii++ )
+      cerr << funcNames[ii] << endl;;
 
-        // Free the string pointers
-        free( funcNames );
-        exit(1);
-    }
+    // Free the string pointers
+    free( funcNames );
+    exit(1);
+  }
 }
 #endif
