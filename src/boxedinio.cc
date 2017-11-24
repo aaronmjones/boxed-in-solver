26f3e6ed2f3ab1502445c3264af29fd1ad0054d3
Aaron <ajones2279@gmail.com>
2017-11-24 14:43:26 -0700

    Windows print-in-color

--- src/boxedinio.cc
+++ src/boxedinio.cc
@@ -9,6 +9,7 @@
 
 #include <cstring>
 #include <chrono>
+#include <iostream>
 
 using namespace std;
 using namespace std::chrono;
@@ -142,7 +143,92 @@ const char* cyan    = "\x1b[1;36m";
 const char* brown   = "\x1b[0;33m";
 const char* white   = "\x1b[1;37m";
 #elif defined(_WIN32)
-#include <ConsoleColor.h>
+#include <windows.h>
+inline std::ostream& blue(std::ostream &s)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout, FOREGROUND_BLUE
+			    | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    return s;
+}
+
+inline std::ostream& red(std::ostream &s)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout,
+			    FOREGROUND_RED | FOREGROUND_INTENSITY);
+    return s;
+}
+
+inline std::ostream& green(std::ostream &s)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout,
+			    FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    return s;
+}
+
+inline std::ostream& yellow(std::ostream &s)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout,
+        FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
+    return s;
+}
+
+inline std::ostream& brown(std::ostream &s)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout,
+			    FOREGROUND_GREEN | FOREGROUND_RED);
+    return s;
+}
+
+inline std::ostream& cyan(std::ostream &s)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout,
+        FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    return s;
+}
+
+inline std::ostream& magenta(std::ostream &s)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout,
+        FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY);
+    return s;
+}
+
+inline std::ostream& white(std::ostream &s)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout,
+	FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+    return s;
+}
+
+inline std::ostream& normal(std::ostream &s)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout,
+        FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    return s;
+}
+
+struct color {
+    color(WORD attribute) : m_color(attribute) {};
+    WORD m_color;
+};
+
+template <class _Elem, class _Traits>
+std::basic_ostream<_Elem, _Traits>&
+operator<<(std::basic_ostream<_Elem, _Traits>& i, color& c)
+{
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hStdout, c.m_color);
+    return i;
+}
 #endif
 
 
